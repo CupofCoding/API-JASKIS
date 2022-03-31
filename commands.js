b26ac73d9095fd9785843878c46c6dc06d067f7d
Cup of Coding <anne@cupofcoding.com>
2022-03-30 18:14:16 -0700

    assignment complete

--- commands.js
+++ commands.js
@@ -9,6 +9,7 @@ use jaskis
 // 2. Create a collection called bounties
 db.createCollection('bounties')
 
+
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
 db.bounties.insertOne('Thanoceros')
@@ -99,7 +100,7 @@ db.bounties.find({location:'Woodlands'}, {species:'Groundhog'})
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-db.bounties.updateOne({name: 'Polarwind'}, $set: {reward: 10000})
+db.bounties.updateOne({name: "Polarwind"},{$set: {reward: 10000}})
 
 // 2. Remove Lokinkajou
 db.bounties.deleteOne({name: "Lokinkajou"})
